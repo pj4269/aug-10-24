31c0356b33573dc3ff9c81fc195bdeb4e1768a93
Micah <contact_micah@yahoo.com>
2024-08-12 07:56:50 -0700

    Made changes to App.js and MyWebcam2.js component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { get } from 'aws-amplify/api';
+import { useState } from 'react'
+import { Amplify } from "aws-amplify";
 
-function App() {
+//import WebCam from './components/MyWebCam';
+import Photo_capture_from_scratch from  './components/MyWebCam2';
+//import UploadPicture from './components/UploadPic'
+//import UploadPicture2 from './components/UploadPic2'
+//import PhotoDisplay from './components/PhotoFromLambda'
+
+//import Photo_capture_from_scratch2 from  './components/MyWebCam3';
+
+
+
+const myAPI = "ApiAug10"
+//const myAPI = "ApiAug08" // aug_08_24/test
+ 
+const path = "/picture"; // .. /{pictureId}
+//const path = "/picture2"
+const myEndpoint = "https://300fh0i2f6.execute-api.us-west-2.amazonaws.com" //  "https://0kl0o417d5.execute-api.us-west-2.amazonaws.com/dev",
+const awsmobile = {
+    "aws_project_region": "us-west-2",
+    "aws_cloud_logic_custom": [
+        {
+            "name": myAPI,
+            "endpoint": myEndpoint,
+            "region": "us-west-2"
+        }
+    ]
+}; 
+/*
+const awsmobile = {
+    "aws_project_region": "us-west-1",
+    "aws_cloud_logic_custom": [
+        {
+            "name": "ApiAug08",
+            "endpoint": "https://ekidbibwk0.execute-api.us-west-2.amazonaws.com/dev",
+            "region": "us-west-1"
+        }
+    ]
+};  */ 
+
+
+
+Amplify.configure(awsmobile);
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [customers, setCustomers] = useState([])
+
+  async function getCustomer(e) {
+    let customerId = e.input
+    const restOperation = get({apiName: myAPI, path: path + "/" + customerId})
+    const { body } = await restOperation.response;
+    const response = await body.json();
+    console.log(response)
+    let newCustomers = [...customers]
+    newCustomers.push(response)
+    setCustomers(newCustomers)
+  }
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="App">         
+     
+      <Photo_capture_from_scratch />
+       
+      <div>
+          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+
+      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       customers.map((thisCustomer, index) => {
+         return (
+        <div key={thisCustomer.customerId}>
+          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+        </div>)
+       })
+      }
+      
+      <h2>1st WebCam: Not working!</h2>
+       <WebCam />   
+      
     </div>
-  );
+  )
 }
 
 export default App;
+
+
