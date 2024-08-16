6dde8a716f290a024a74aea1331b73c77144c9f2
Micah <contact_micah@yahoo.com>
2024-08-16 10:31:21 -0700

    testing-4

--- src/components/MyWebCam2.js
+++ src/components/MyWebCam2.js
@@ -338,7 +338,7 @@ function Photo_capture_from_scratch() {
   const timestamp = Date.now();
   formData2.append("file", capturedFile, `${timestamp}_${capturedFile.name}`);  
   //console.log( "Hi ", formData2 )
-  const data = {
+  const requestOptions = {
     method: "GET",
     body: formData2
   };
@@ -348,18 +348,19 @@ function Photo_capture_from_scratch() {
   
   //try {
                      //              https://300fh0i2f6.execute-api.us-west-2.amazonaws.com
-    //const response = await fetch('https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/', requestOptions); 
+    const response = await fetch('https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/', requestOptions); 
 
 
         //const response = await fetch(`https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/${data}`,     
         //const response = await fetch(`https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/${data}`);
+        /*
         const response = await fetch(`https://ekidbibwk0.execute-api.us-west-2.amazonaws.com/dev/${data}`, 
         {
         method: "GET",
         headers: {
         "Content-Type": "application/json"
           }
-        });    
+        }); */ 
           
     const result = await response.json();
     console.log("Response from server2:", result);    
