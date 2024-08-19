52c98ebd0feba886580f30f07c5de5156aa0d1a3
Micah <contact_micah@yahoo.com>
2024-08-19 07:28:58 -0700

    sending user_id

--- src/components/MyWebCam2.js
+++ src/components/MyWebCam2.js
@@ -358,11 +358,24 @@ function Photo_capture_from_scratch() {
   */
   
   //try {
-                     //              https://300fh0i2f6.execute-api.us-west-2.amazonaws.com
+                     // This works - Aug 19, 24
+                     /*
     const response = await fetch('https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture2/', requestOptions); 
+    const result = await response.json();
+    console.log("Response from server2:", result); 
+    */
+    // new: 
+    const userId = '12345'; // replace with your actual test user_id
+    const url = `https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/${userId}`;
 
+    try {
+							const response = await fetch(url, requestOptions);
+							const data = await response.json(); // or response.text(), depending on your response type
+							console.log(data); // handle the response data
+						} catch (error) {
+							console.error('Error:', error);
+						}
 
-        //const response = await fetch(`https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/${data}`,     
         //const response = await fetch(`https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/${data}`);
         /*
         const response = await fetch(`https://ekidbibwk0.execute-api.us-west-2.amazonaws.com/dev/${data}`, 
@@ -373,8 +386,7 @@ function Photo_capture_from_scratch() {
           }
         }); */ 
           
-    const result = await response.json();
-    console.log("Response from server2:", result);    
+   
 
     /*
     const response = await fetch( 'https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/', {
@@ -412,7 +424,7 @@ function Photo_capture_from_scratch() {
 
     const sendData = async () => {
       try {
-        const response = await fetch(`https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/${data}`, 
+        const response = await fetch(`https://300fh0i2f6.execute-api.us-west-2.amazonaws.com/dev/picture/${data}/requestOptions`, 
         //const response = await fetch(`https://ekidbibwk0.execute-api.us-west-2.amazonaws.com/dev/${data}`, 
         {
         method: "GET",
