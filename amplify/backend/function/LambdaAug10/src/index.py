de9e7f4fd64693d2d1a3e08918f1cffc39e85e92
Micah <contact_micah@yahoo.com>
2024-08-20 14:35:19 -0700

    import my_pillow.PIL  in index.py - seems to be working. Testing-2

--- amplify/backend/function/LambdaAug10/src/index.py
+++ amplify/backend/function/LambdaAug10/src/index.py
@@ -4,9 +4,17 @@ import pandas as pd
      
    
 def handler(event, context):
+
+  # initialize list of lists
+  data = [['tom', 10], ['nick', 15], ['juli', 14]]
+  # Create the pandas DataFrame
+  df = pd.DataFrame(data, columns=['Name', 'Age'])
+  # print dataframe.
+  print(df)
+
   #print('received event: ', event)
-  image = Image.new('RGB', (100, 100), color = 'red')
-  print ("Pillow image - Aug 20, 24: ", type(image)  )
+  #image = Image.new('RGB', (100, 100), color = 'red')
+  #print ("Pillow image - Aug 20, 24: ", type(image)  )
   get_raw_path    = ''
   create_raw_path = '/postP'
   #target_resource = '/picture/{pictureId}'
